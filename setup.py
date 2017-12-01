62dc776c4020fd1b922d8446e8d9a113e824e537
malithmanuhara <malithmanuhara@gmail.com>
2017-12-01 16:45:36 +0530

    edit

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 setup(name='',
       version='',
       description='',
-      author='Per Jonsson',
-      author_email='per.d.jonsson@gmail.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask', 'WTForms'],
+      author='Malith Manuhara',
+      author_email='malithmanuhara@gmail.com',
+      url='https://github.com/malithmanuhara/chat-bot',
+      install_requires=extract_requirements('requirements.txt'),
+	  data_files=[('requirements.txt', ['requirements.txt'])],
      )
