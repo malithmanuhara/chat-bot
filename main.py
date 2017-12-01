9ec317b75904f1a339f2f6d92d6591660077eef4
malithmanuhara <malithmanuhara@gmail.com>
2017-12-01 17:09:38 +0530

    test1

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from script import validate
 
 # App config.
 DEBUG = True
-application = Flask(__name__)
-application.config.from_object(__name__)
-application.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
 class ReusableForm(Form):
     name = TextField('Name:', validators=[validators.required()])
 
 
-@application.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def hello():
     form = ReusableForm(request.form)
 
@@ -32,6 +32,7 @@ def hello():
             flash('Error: All the form fields are required. ')
 
     return render_template('hello.html', form=form)
-	
+
+
 if __name__ == "__main__":
-    application.run()
+    app.run()
